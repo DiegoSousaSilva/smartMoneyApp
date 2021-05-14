3a8306c7ffd27e1b0fbb48346d43c8dd2e60d88d
Diego Sousa <derickbenji2@gmail.com>
2021-05-14 00:39:22 -0300

    Dispositivos :: Geocoding: formatando localização

--- src/pages/NewEntry/NewEntryAddressPicker/index.js
+++ src/pages/NewEntry/NewEntryAddressPicker/index.js
@@ -2,26 +2,52 @@ import React from 'react';
 import {View, TouchableOpacity, StyleSheet, Alert} from 'react-native';
 
 import Geolocation from '@react-native-community/geolocation';
+import Geocoder from 'react-native-geocoding';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Colors from '../../../components/styles/Colors';
 
 // import { Container } from './styles';
 
 const NewEntryAddressPicker = () => {
-  const onButtonPress = () => {
+  const getLocation = (latitude, longitude) => {
+    Geocoder.init('AIzaSyC6zpxiveenHn5riKB_VbSh3X28eRAqeH0');
+
+    Geocoder.from({latitude: latitude, longitude: longitude})
+      .then(json => {
+        const formatedAddress = json.results[0].formatted_address;
+
+        Alert.alert('Formated  Address:', formatedAddress);
+      })
+      .catch(error => {
+        console.error(
+          'NewEntryAddressPicker :: GeoLocation: error on get address',
+          error,
+        );
+        Alert.alert('Houve um erro ao recuparar sua Localização');
+      });
+  };
+
+  const getPosition = () => {
     Geolocation.getCurrentPosition(
       pos => {
         const latitude = pos.coords.latitude;
         const longitude = pos.coords.longitude;
 
-        const position = `Lat. ${latitude} Lon. ${longitude}`;
-
-        Alert.alert('posição', position);
+        //console.log(`Lat ${latitude} Lon ${longitude}`);
+        getLocation(latitude, longitude);
       },
       error => {
-        console.error('NewEntryAddressPicker :: error on get address', error);
+        console.error(
+          'NewEntryAddressPicker :: GetPosition: error on get address',
+          error,
+        );
+        Alert.alert('Houve um erro ao recuparar sua posição');
       },
     );
+  };
+
+  const onButtonPress = () => {
+    getPosition();
   };
 
   return (
